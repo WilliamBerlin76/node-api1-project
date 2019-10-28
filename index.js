4697990cbee3e0c964da133dac08094bbabe83f9
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-28 14:46:20 -0500

    server running and listening to port 8000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+const port = 8000;
+server.listen(port, () => console.log('\n=== API on port 8000 ===\n'));
